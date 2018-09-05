ce5fd64375f12427ca5d7eaa86cc5abaa7350e48
or1205 <@1.235.51.187>
2018-09-05 22:24:03 +0900

    변수에 계산값을 저장하고 출력

---
+++ src/hello/구구단2.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class 구구단2 {
+	public static void main(String[] agrs) {
+		int result = 4 * 1;
+		System.out.println(result);
+		
+		result = 4 * 2 ;
+		System.out.println(result);
+		
+	}
+
+}
