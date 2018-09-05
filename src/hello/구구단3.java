22793138e60f6807dda2e3de5afe617d315d58dc
or1205 <@1.235.51.187>
2018-09-05 22:45:03 +0900

    "Scanner"이용한 구구단 입력받고 출력

---
+++ src/hello/구구단3.java
@@ -0,0 +1,35 @@
+package hello;
+
+import java.util.Scanner;
+
+public class 구구단3 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println(" 숫자를 입력하시오.");
+		int input = scan.nextInt();
+		System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡ");
+		System.out.println(input * 1);
+		System.out.println(input * 2);
+		System.out.println(input * 3);
+		System.out.println(input * 4);
+		System.out.println(input * 5);
+		System.out.println(input * 6);
+		System.out.println(input * 7);
+		System.out.println(input * 8);
+		System.out.println(input * 9);
+
+
+		
+		
+		
+		
+	
+	
+		
+		
+		
+	}
+	
+
+}
